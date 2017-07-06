bc90e242fce41b307a07aa6243be709a4bfcf17b
aathi <aathi@7zero.com>
2017-07-06 11:46:36 +0200

    ApolloClient added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { AppBar, Drawer, List, ListItem } from 'material-ui';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo'
 import Index from './components';
 
+
+const networkInterface = createNetworkInterface({
+// __SIMPLE_API_ENDPOINT__ looks similar to: `https://api.graph.cool/simple/v1/<PROJECT_ID>`
+  uri: '__SIMPLE_API_ENDPOINT__'
+});
+const client = new ApolloClient({networkInterface})
+
 export default () => (
+  <ApolloProvider client={client}>
     <Router>
          <Route exact path="/" component={Index}/>
     </Router>
+  </ApolloProvider>
 );
